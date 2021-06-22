81840dd0ddc131f0ed2fbbb96bf1d592214055d5
Nurbek <nurimankg@yahoo.com>
2021-06-22 18:56:15 +0000

    u

--- dev/main.tf
+++ dev/main.tf
@@ -1,12 +1,3 @@
 module "s3_module" {
     source = "github.com/nurimankg/tf_modules/module/s3"
-    env = "dev"
 }
-
-# output "bucket_name" {
-#     value = module.s3_module.s3_name
-# }
-
-# output "bucket_arn" {
-#     value = module.s3_module.s3_arn
-# }
