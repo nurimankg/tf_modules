ab19f7e8106c9fb5758351a1a348a3682fdbc901
Nurbek <nurimankg@yahoo.com>
2021-06-22 20:28:18 +0000

    u

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,10 @@
 module "s3_module" {
   source = "github.com/nurimankg/tf_modules/module/s3"
+  cidr_block = var.cidr
   env    = var.env
 }
 
 module "vpc" {
   source = "github.com/nurimankg/tf_modules/module/vpc"
-  env    = var.env
+  # env    = var.env
 }
