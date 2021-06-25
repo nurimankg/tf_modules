8c0c29b3570468add70d9be5717d3c7824a1070d
Nurbek <nurimankg@yahoo.com>
2021-06-25 23:48:20 +0000

    u

--- module/s3/s3.tf
+++ module/s3/s3.tf
@@ -1,6 +1,7 @@
   resource "aws_s3_bucket" "main" {
     bucket = "${var.env}-backend-tfvars"
-    acl = "private"
+    key    = "dev/terraform.tfvars"
+    region = "us-east-1"
     tags = local.common_tags
 }
 
