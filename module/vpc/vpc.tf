dde568b19e7425ea6ea95ba51af690178ecd1ae9
Nurbek <nurimankg@yahoo.com>
2021-06-26 01:23:33 +0000

    u

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -3,12 +3,11 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block           = var.cidr
   enable_dns_hostnames = true
+  tags = local.common_tags
 
-
-  tags            = merge(
-     local.common_tags,
-     {
-       Name = "var.vpc_name"
-     }
-  )
+#   tags = {
+#     Name = "Main_vpc"
+#     Tean = "devops"
+#     Owner = "Nur"
+#   }
 }
